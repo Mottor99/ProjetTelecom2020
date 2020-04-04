202532f079a52f9cd9e10fdeced8e48e7ce85531
nicolas647 <nicolasmaton8@gmail.com>
2020-04-04 11:28:53 +0200

    Merge branch 'master' of https://github.com/Mottor99/ProjetTelecom2020
     Conflicts:
    	Code/Main.py

--- Code/Room.py
+++ Code/Room.py
@@ -1,3 +1,17 @@
 class Room:
 
     def __init__(self):
+        self.liste_walls
+        self.liste_rays
+        self.transmitter
+        self.reciever
+
+    def ray_tracing(self, transmittor, reciever):
+
+        return liste_rays
+
+    def calculate(self, transmittor, reciever):
+
+        return
+
+    def affichage_graphique(self):
