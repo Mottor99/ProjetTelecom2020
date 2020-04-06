7b978b9a25d789008c358cb9db8377b4bf2549d1
nicolas647 <nicolasmaton8@gmail.com>
2020-04-06 21:26:02 +0200

    Merge branch 'master' of https://github.com/Mottor99/ProjetTelecom2020
     Conflicts:
    	Code/Main.py

--- Code/room.py
+++ Code/room.py
@@ -16,12 +16,12 @@ class Room:
     def ray_tracing(self, transmittor, reciever, liste_walls):
         liste_rays = []
         sous_liste_mur0 = []
-        ray0 = ray(sous_liste_mur0)
+        ray0 = creation_ray(sous_liste_mur0)
         liste_rays.append(ray0)
         for i in liste_walls :
             sous_liste_mur1 = []
             sous_liste_mur1.append(i)
-            ray1 = ray(sous_liste_mur1)
+            ray1 = creation_ray(self, sous_liste_mur1)
             liste_rays.append(ray1)
             for j in liste_walls :
                 if j == i :
@@ -29,7 +29,7 @@ class Room:
                 sous_liste_mur2 = []
                 sous_liste_mur2.append(i)
                 sous_liste_mur2.append(j)
-                ray2 = ray(sous_liste_mur2)
+                ray2 = creation_ray(self, sous_liste_mur2)
                 liste_rays.append(ray2)
                 for k in liste_walls :
                     if k == i or k == j :
@@ -38,7 +38,7 @@ class Room:
                     sous_liste_mur3.append(i)
                     sous_liste_mur3.append(j)
                     sous_liste_mur3.append(k)
-                    ray3 = ray(sous_liste_mur3)
+                    ray3 = creation_ray(self, sous_liste_mur3)
                     liste_rays.append(ray3)
         return liste_rays
 
@@ -63,14 +63,20 @@ class Room:
 
         return 0
 
-    def ray(sous_liste_mur, emetteur, recepteur):
+    def creation_ray(self, sous_liste_mur, emetteur, recepteur):
         point = emetteur.position
         liste_images = []
         for i in sous_liste_mur:
             point = image(point, i)
             liste_images.append(point)
-        for j in range len(liste_image)
-            ...
+        for j in range len(liste_image-1):
+            ray.liste_points.append(recepteur.position)
+            if i == 0 :
+                droite = line(recepteur.position, liste_image[len(liste_image-1)])
+                point = droite.intersection(sous_liste_mur[len(liste_image-1)].droite)
+                ray.liste_points.append(point)
+            else :
+
 
 
         return 0
