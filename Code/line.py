83d0b48073dce9da2c9185f033770e766d190975
nicolas647 <nicolasmaton8@gmail.com>
2020-04-09 19:50:53 +0200

    gros problème quand la droite est verticale

--- Code/line.py
+++ Code/line.py
@@ -10,12 +10,26 @@ class Line:
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
-        self.a = (self.point1[2]-self.point2[2])/(self.point1[1]-self.point2[1])
-        self.b = self.point1[2]-self.a*self.point1[1]
+        if (self.point1[0]-self.point2[0]) == 0:
+            self.a = 100000
+            self.b = point1[0]
+        else:
+             self.a = (self.point1[1]-self.point2[1])/(self.point1[0]-self.point2[0])
+             self.b = self.point1[1]-self.a*self.point1[0]
 
 
     def intersection(self, droite_intersectee):
-        if ((self.a-droite_intersectee.a)/self.a) < 0.01 :
+        if self.a == 100000:
+            if droite_intersectee.a == 100000:
+                x = y = -1
+            else:
+                x = self.b
+                y = droite_intersectee.a * x + droite_intersectee.b
+        elif droite_intersectee.a == 100000:
+            x = droite_intersectee.b
+            y = self.a * x + self.b
+
+        elif ((self.a-droite_intersectee.a)/self.a) < 0.01 :
             x = y = -1
         else :
             x = (droite_intersectee.b - self.b)/(self.a - droite_intersectee.a)
